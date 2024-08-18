c47b58475b26e626ee44ce247036a0c96926c604
DeepEnd007 <yihuihu56@gmail.com>
2024-08-18 23:57:28 +1200

    second commit of today

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,5 +1,26 @@
-function Stairs() {
+import { motion } from "framer-motion";
+
+//variants
+
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+//calculate the reverse index fot staggred delay
+const reverseIndex = (index) => {
+  const totalSteps = 0;
+};
+
+const Stairs = () => {
   return <div>Stairs</div>;
-}
+};
 
 export default Stairs;
