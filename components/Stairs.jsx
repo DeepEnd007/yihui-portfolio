74d2ec8c5e0639ca5ec55116bdce4acb0f0d3355
DeepEnd007 <yihuihu56@gmail.com>
2024-08-19 23:03:37 +1200

    working on stairs

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -17,10 +17,33 @@ const stairAnimation = {
 //calculate the reverse index fot staggred delay
 const reverseIndex = (index) => {
   const totalSteps = 0;
+  return totalSteps - index - 1;
 };
 
 const Stairs = () => {
-  return <div>Stairs</div>;
+  return (
+    <>
+      {/* {render 6 motion divs, each representing a step of the stairs.
+    Each div will have the same animation by the stairAnimation object.
+    The delay for each div is calculated sinamically based on it's reversed index,
+    creating a staggred effect with decreasing delay for each subsequent step.} */}
+      {[...Array(6)].map((_, index) => {
+        <motion.div
+          key={index}
+          variants={stairAnimation}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+            delay: reverseIndex(index) * 0.1,
+          }}
+          className="h-full w-full bg-white relative"
+        ></motion.div>;
+      })}
+    </>
+  );
 };
 
 export default Stairs;
