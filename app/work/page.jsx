4c7ed525ce670be6efdeb8323c2cb3953e2387d3
DeepEnd007 <yihuihu56@gmail.com>
2024-08-27 21:45:43 +1200

    start working on work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,53 @@
-function Work() {
-  return <div>work page</div>;
-}
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper.css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description: "An online shopping site for home and living goods",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
+    description: "An online shopping site for home and living goods",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description: "An online shopping site for home and living goods",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
+
+const Work = () => {
+  return <div>Work page</div>;
+};
 
 export default Work;
